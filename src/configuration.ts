e8e9c16afccf1a30ae5467f02cf6d1560293bef6
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2021-10-12 04:16:31 +0200

    feat: add new properties to DokapiConfig

--- src/configuration.ts
+++ src/configuration.ts
@@ -12,12 +12,24 @@ import schema from "./schema/configuration.schema.json";
 // CONSTANTS
 const kSchemaValidator = new Ajv();
 
+/** String primitive containing a relative path to a file */
+export type DokapiFilePath = string;
+
 export interface DokapiMenu {
   /** Title of the page (Will be extracted from markdown if not provided) */
   title?: string;
 
   /** System path to page or the directory containing the page */
-  path: string;
+  path: DokapiFilePath;
+
+  /** HTTP Configuration (examples & schemas) */
+  http?: {
+    jsonSchema?: DokapiFilePath;
+    jsonResponse: DokapiFilePath;
+  };
+
+  /** Code examples (nodejs, typescript etc) */
+  code?: DokapiFilePath | Record<string, DokapiFilePath>;
 
   /** Is the menu collapsable (see stripe API for an example) */
   collapsable?: boolean;
@@ -42,14 +54,21 @@ export type DokapiSubConfiguration = Pick<DokapiConfiguration, "title"> & {
   menu: DokapiSubMenu[]
 };
 
+export interface ReadOptions {
+  /** Default: `toml` */
+  format?: "toml" | "json";
+}
+
 /**
  * @description Read, parse and validate a Dokapi configuration.
  */
-export async function read(location: string): Promise<DokapiConfiguration> {
+export async function read(location: string, options: ReadOptions = {}): Promise<DokapiConfiguration> {
+  const { format = "toml" } = options;
+
   const str = await fs.readFile(
-    path.join(location, "dokapi.config.toml"), "utf-8"
+    path.join(location, `dokapi.config.${format}`), "utf-8"
   );
-  const manifest = TOML.parse(str);
+  const manifest = format === "toml" ? TOML.parse(str) : JSON.parse(str);
 
   const validate = kSchemaValidator.compile(schema);
   if (!validate(manifest)) {
